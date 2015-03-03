d20aa58b434511486f8a6d9f75bf4ebd87053404
katarina <golbang.k@gmail.com>
2015-03-03 22:26:17 +0100

    additional validations; errors partial

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :creator, :foreign_key => 'user_id', :class_name => 'User'
   belongs_to :post
+
+  validates :body, :presence => true
+  validates_length_of :body, minimum: 3, tokenizer: Proc.new { |body| body.split(/\w+/) }, too_short: 'is too short. Post a meaningful message.'
+  validates_length_of :body, maximum: 100, tokenizer: Proc.new { |body| body.split(/\w+/) }, too_long: 'is too long. This is not an essay.'
+  validate :suitable_body_content
+
+  private
+  def suitable_body_content
+    if body =~ /(some|bad|words|here)/i
+      errors.add(:body, 'has been declined. Think twice.')
+    end
+  end
 end
