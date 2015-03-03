c0ac213fbe09082e0364563cf3f5ab5f7dfe2ef9
katarina <golbang.k@gmail.com>
2015-03-03 21:59:15 +0100

    lecture https://www.gotealeaf.com/lessons/64663c29/assignments/1931

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+
+    if @comment.save
+      flash[:notice] = 'Your comment was added'
+      redirect_to post_path(@post)
+    else
+      # no post_path because this would be an url
+      # we need a template
+      render 'posts/show'
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update]
+  # 1. set up instance variable for action
+  # 2. redirect based on some condition
+
   def index
     @posts = Post.all
     respond_to do |format|
@@ -8,6 +13,41 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Your post was created.'
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = 'The post has been updated'
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit!
+  end
+
+  def set_post
     @post = Post.find(params[:id])
   end
 end
