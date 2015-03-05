42dc5b8bfafb8b5c6b3bf093ca0f8796ccb1cb3c
katarina <golbang.k@gmail.com>
2015-03-05 10:13:54 +0100

    category integratin: https://www.gotealeaf.com/lessons/64663c29/assignments/1698

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,26 @@
+class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = 'Category has been saved'
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @categories = Category.all
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
