1c8051947115df5854dc5881c9420c437afff759
katarina <golbang.k@gmail.com>
2015-03-11 23:05:35 +0100

    ajax votes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,11 +21,23 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     @vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
     if @vote.valid?
-      flash[:notice] = 'Your vote was counted.'
+      notice = 'Your vote was counted.'
     else
-      flash[:error] = 'You can vote <string>that</string> only once.'.html_safe
+      error = 'You can only vote for that once.'
     end
-    redirect_to :back
+    respond_to do |format|
+      format.html do
+        flash[:notice] = notice if notice
+        flash[:error] = error if error
+        redirect_to :back
+      end
+      format.json do
+        hash = { count: @comment.total_votes }
+        hash[:notice] = notice if notice
+        hash[:error] = error if error
+        render json: hash
+      end
+    end
   end
 
   private
